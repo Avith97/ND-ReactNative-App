90c42807504343ac68fc11d563ae31507a1bb6ea
Laxman Nemane <laxmann@IISPLPDC.LOCAL>
2025-04-22 02:07:32 +0530

    added step chart component

--- src/utils/constants/Images.js
+++ src/utils/constants/Images.js
@@ -16,18 +16,15 @@ export const Images = {
   belly3:require(ImgPath + "/onboardImages/Belly/belly3.png"),
   belly4:require(ImgPath + "/onboardImages/Belly/belly4.png"),
   belly5:require(ImgPath + "/onboardImages/Belly/belly5.png"),
-
-
   runner_bg_image: require(ImgPath + "/runner_bg_img.png"),
 
 
   // consent Images
-
   google_fit:require(ImgPath + "/ConcentImages/googlefit.png"),
   fitbit:require(ImgPath + "/ConcentImages/fitbit.png"),
   garmin:require(ImgPath + "/ConcentImages/Garmin.png"),
   mapmyrun:require(ImgPath + "/ConcentImages/mapmyrun.png"),
-  strava:require(ImgPath + "/ConcentImages/strava.png"),
+  // strava:require(ImgPath + "/ConcentImagesr/strava.png"),
 
 
 
