d6f42224f2db63274a7cc8ad63504d976b39ffe8
Laxman Nemane <laxmann@IISPLPDC.LOCAL>
2025-04-22 00:10:22 +0530

    made change in customdropdown and customdatepicker, and editscreen developed

--- src/screens/profile/EditProfileScreen.js
+++ src/screens/profile/EditProfileScreen.js
@@ -1,8 +1,45 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import EditProfileScreenUI from './EditProfileScreenUI';
 
-export default function EditProfileScreen() {
+export default function EditProfileScreen(props) {
+
+   const [state, setstate] = useState({})
+      const [options, setoptions] = useState({})
+      const [err, seterr] = useState(null)
+  
+      useEffect(() => {
+          console.log('state render-->', state)
+      }, [state])
+  
+  
+      function handleChange(params, val) {
+          console.log('params', params)
+          console.log('val', val)
+          setstate({
+              ...state,
+              [params]: val
+          })
+      }
+  
+      function validation(params) {
+          let isValid = true;
+          let err = {}
+  
+          return isValid
+  
+      }
+  
+      function handleSubmit(params) {
+          let valid = validation()
+          if (!valid) { return }
+          try {
+  
+          } catch (error) {
+              appsnackbar.showErrMsg('Something went wrong')
+          }
+      }
+  
   return (
     <View style={{flex: 1, padding: 20 , backgroundColor:"#fff"}}>
       <EditProfileScreenUI />
