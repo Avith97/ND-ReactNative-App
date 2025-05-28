f52f0721f2f737bc36acee0ff2b4a3f8baca9a03
Laxman Nemane <laxmann@IISPLPDC.LOCAL>
2025-05-28 15:14:48 +0530

    added code for Android the SSL bypass certificate

--- src/screens/splash/SplashScreen.js
+++ src/screens/splash/SplashScreen.js
@@ -3,8 +3,6 @@ import React, {useEffect, useState} from 'react';
 import SplashUI from './SplashUI';
 import Strings from '../../utils/constants/Strings';
 import {appsnackbar} from '../../common/functions/snackbar_actions';
-// import https from 'https';
-import axios from 'axios';
 
 const SplashScreen = props => {
   const [state, setState] = useState({
@@ -16,7 +14,7 @@ const SplashScreen = props => {
     setTimeout(() => {
       if (!state.isLoggedIn) {
         props.navigation.replace(Strings.NAVIGATION.app, {
-          isLoggedIn:false,
+          isLoggedIn: false,
         });
       }
       //   props.navigation.replace(Strings.NAVIGATION.auth);
@@ -27,25 +25,6 @@ const SplashScreen = props => {
   }, []);
 
   async function checkAppLaunch(params) {
-    // getdevice()
-    axios.get('https://192.168.1.49:8443/api/v1/yoga/challenge/leaderboard/details/435',
-      {
-        timeout: 10000,
-      },
-    )
-      .then(function (response) {
-        console.log(response, '<==== response ====>');
-
-        alert('response success');
-      })
-      .catch(function (error) {
-        alert('response error');
-        console.log(
-          error,
-          '<==== error ====>',
-          JSON.parse(JSON.stringify(error)),
-        );
-      });
     let x = await Linking.getInitialURL();
     if (x) {
       console.log('<==== new console ====>', x);
