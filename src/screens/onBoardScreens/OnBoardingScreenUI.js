1a2ccf878402ecfcd35a89d09f9e1b07858b729b
vinitANS <vinit@anssoft.in>
2025-06-02 17:23:09 +0530

    changes:loader added

--- src/screens/onBoardScreens/OnBoardingScreenUI.js
+++ src/screens/onBoardScreens/OnBoardingScreenUI.js
@@ -1,29 +1,39 @@
 import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import OnboardingWrapper from '../../common/components/onboarding/OnboardingWrapper';
 import MoreAboutScreen from './MoreInfo/MoreAboutScreen';
 import {hp, wp} from '../../common/functions/dimensions';
+import Loader from '../../common/components/loader/Loader';
 
 const OnBoardingScreenUI = props => {
+  const [key, setkey] = useState(0)
+  const [slides, setslides] = useState([
+    () => (
+      <MoreAboutScreen
+        {...props}
+        handleChange={props.handleChange}
+        {...styles}
+      />
+    )
+  ])
+
+  useEffect(() => {
+    setkey(key + 1);
+    setslides([...slides, ...props?.slides])
+  }, [props?.slides])
+
+
   return (
-    <View style={{flex: 1}}>
-      {props.slides?.length &&
-        <OnboardingWrapper
-        slides={[
-          () => (
-            <MoreAboutScreen
-              {...props}
-              handleChange={props.handleChange}
-              {...styles}
-            />
-          ),
-          ...props.slides,
-        ]}
+    <View key={key} style={{ flex: 1 }}>
+      <Loader />
+      {/* {props.slides?.length && */}
+      <OnboardingWrapper
+        slides={[...slides]}
         onNext={props.onNext}
         onSkip={() => console.log('Skipped')}
         onFinish={props.handleSubmit}
       />
-      }
+      {/* } */}
     </View>
   );
 };
