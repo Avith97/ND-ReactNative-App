0278c3994e59e742551daf85caea0be87038d820
Laxman1628 <laxmann@interfaceinfosoft.com>
2025-06-04 08:06:24 +0530

    added event save reducer : done

--- src/redux/reducers/allReducers.js
+++ src/redux/reducers/allReducers.js
@@ -4,11 +4,13 @@ import AuthReducer from "./AuthReducer";
 import UserReducer from "./UserReducer";
 import ThemeReducer from "./ThemeReducer";
 import OnBoardingReducer from "./OnBoardingReducer";
+import eventReducer from "./eventReducer";
 
 export const allReducers = combineReducers({
     auth: AuthReducer,
     user: UserReducer,
     settings: SettingReducer,
     theme: ThemeReducer,
-    onboard: OnBoardingReducer
+    onboard: OnBoardingReducer,
+    eventData:eventReducer
 })
