c5ef7cc1dc755a5791598fd4448bead65e103bc3
vinitANS <vinit@anssoft.in>
2025-06-05 16:24:46 +0530

    added:toasts

---
+++ src/common/components/toasts/toast_types.js
@@ -0,0 +1,16 @@
+export const toast_types = {
+  success: 'success',
+  error: 'error',
+  tomato_toast: 'tomato_toast',
+  custom: 'custom',
+  add_goals: 'add_goals',
+  bottom_sheet: 'bottom_sheet',
+  add_new_goals: 'add_new_goals',
+  regulatory_info: 'regulatory_info',
+  show_nominee: 'show_nominee',
+  logout_bottomsheet: 'logout_bottomsheet',
+  cart_sip_onetime_modal: 'cart_sip_onetime_modal',
+  otp_modal: 'otp_modal',
+  filter_modal: 'filter_modal',
+  upi_timer: 'upi_timer'
+}
