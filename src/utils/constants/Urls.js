8aa01d35b8111fd346b4d72c097a6c800b59a8ed
vinitANS <vinit@anssoft.in>
2025-06-05 16:40:34 +0530

    minor changes

--- src/utils/constants/Urls.js
+++ src/utils/constants/Urls.js
@@ -12,7 +12,7 @@ export const BASE_URL = SERVER[environment];
 export const URL = {
   google: 'www.google.in',
   otp: 'login/otp',
-  otp_verify: 'login/verify/otp', //{userName: "vinit@anssoft.in", otp: "122222", distKey: "TXK8vWYRPSXm08uXrZYV0g=="}//{code: "401", verbose: "Invalid OTP"}}
+  otp_verify: 'login/verify/otp', 
   create_profile: 'signup',
 
   //delete user
