1038935919816ec74031da2ed2241b22e212e2b2
vinitANS <vinit@anssoft.in>
2025-06-14 15:52:17 +0530

    changes:json name

--- android/app/src/main/java/com/events/necessarydevilapp/MainActivity.kt
+++ android/app/src/main/java/com/events/necessarydevilapp/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "healthconnectapp"
+  override fun getMainComponentName(): String = "necessary-devil"
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     // In order to handle permission contract results, we need to set the permission delegate.
